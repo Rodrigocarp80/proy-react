df25bd2b392931d089ff1ce5678de4a3e47d4656
Rodrigo <107121727+Rodrigocarp80@users.noreply.github.com>
2023-03-12 17:03:31 -0300

    Add files via upload

--- src/components/Navbar/Categorias/Categorias.jsx
+++ src/components/Navbar/Categorias/Categorias.jsx
@@ -1,33 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Categorias = () => {
     return (
-        <>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Marcas
+        <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+               Vehiculos
         </a>
         <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Ford</a></li>
-            <li><a className="dropdown-item" href="#">Toyota</a></li>
-            <li><a className="dropdown-item" href="#">Nissan</a></li>
-            <li><a className="dropdown-item" href="#">Mercedes</a></li>
-            <li><a className="dropdown-item" href="#">Renault</a></li>
-            <li><a className="dropdown-item" href="#">Audi</a></li>
-            <li><a className="dropdown-item" href="#">BMW</a></li>
-            <li><a className="dropdown-item" href="#">Alfa Romeo</a></li>
-            <li><a className="dropdown-item" href="#">Chevrolet</a></li>
-            <li><a className="dropdown-item" href="#">Citroën</a></li>
-            <li><a className="dropdown-item" href="#">Volkswagen</a></li>
-            <li><a className="dropdown-item" href="#">Fiat</a></li>
-            <li><a className="dropdown-item" href="#">Peugeot</a></li>
-            <li><a className="dropdown-item" href="#">Jeep</a></li>
-            <li><a className="dropdown-item" href="#">Honda</a></li>
-            <li><a className="dropdown-item" href="#">Mitsubishi</a></li>
-            <li><a className="dropdown-item" href="#">Suzuki</a></li>
+            <li><Link className="dropdown-item" to={"/categoria/1"}>0 Km</Link></li>
+            <li><Link className="dropdown-item" to={"/categoria/2"}>Usados</Link></li>
+            <li><Link className="dropdown-item" to={"/"}>Todos</Link></li>
+           
         </ul>
-        </li>
-        <li className="nav-item">
-        </li>
-        </>
+    </li>
     );
 }
 
